ab4dd75e7e1b49f4c011410c9cb203433092f78e
Tambi <tambi.asawo@verticalmotion.ca>
2024-11-05 16:12:39 -0800

    feat: add qr code

---
+++ app/_components/QRCode/QRCode.tsx
@@ -0,0 +1,14 @@
+// Import the QRCode component
+import QRCode from "react-qr-code";
+
+const QRCodeDisplay = ({ url }: { url: string }) => {
+  return (
+    <div style={{ textAlign: "center", margin: "20px" }}>
+      <h3>Scan this QR Code</h3>
+      <QRCode value={url} size={200} />
+      <p>{url}</p>
+    </div>
+  );
+};
+
+export default QRCodeDisplay;
