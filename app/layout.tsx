daee61b54e563aacfcbd5a5b8096290524c97c04
Tambi <tambi.asawo@verticalmotion.ca>
2024-11-05 16:15:57 -0800

    fix: general settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Self Verification | Rented123",
-  description: "Scan your ID and verify photo ",
+  title: "ID Verification | Rented123",
+  description: "Scan your ID and verify yourself ",
 };
 
 export default function RootLayout({
