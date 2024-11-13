2947b522cdd870bc4b5ed4be61abc10d38efa7d1
Tambi <tambi.asawo@verticalmotion.ca>
2024-11-13 09:25:09 -0800

    fix css

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,20 @@
-
 import Link from "next/link";
 
 const Custom404 = () => {
   return (
-    <div className={"error-container"}>
-      <h1>404 - Page Not Found</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        height: "100vh",
+        textAlign: "center",
+        margin: "40px auto",
+        maxWidth: "80%",
+      }}
+    >
+      <h1 style={{ color: "red" }}>Page Not Found</h1>
       <p style={{ lineHeight: "1.6" }}>
         We're sorry, but the page you're looking for doesn't exist, or the link
         is invalid. If your access token has expired, you can purchase the
