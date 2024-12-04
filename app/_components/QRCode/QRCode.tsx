a8b373ea7aa21538e848c04f88186f72f41f5521
Tambi <tambi.asawo@verticalmotion.ca>
2024-12-03 16:41:39 -0800

    fix: css andnew prop

--- app/_components/QRCode/QRCode.tsx
+++ app/_components/QRCode/QRCode.tsx
@@ -1,12 +1,14 @@
 // Import the QRCode component
 import QRCode from "react-qr-code";
 
-const QRCodeDisplay = ({ url }: { url: string }) => {
+const QRCodeDisplay = ({ url, token }: { url: string; token: string }) => {
   return (
-    <div style={{ textAlign: "center", margin: "20px" }}>
-      <h3>Scan this QR Code</h3>
+    <div
+      className="flex flex-col justify-center align-middle items-center gap-2"
+    >
+      <h3 className="font-bold mb-2">Scan this QR Code</h3>
       <QRCode value={url} size={200} />
-      <p>https://services.idscan.rented123.com</p>
+      <p>{`https://services.idscan.rented123.com/${token}`}</p>
     </div>
   );
 };
